ba1ac4471957875b2add44d4281b64c33f707cf5
roopakgupta <roopak17gupta@gmail.com>
2022-03-15 12:58:02 +0530

    initial scss

---
+++ client/src/Components/HomeHeader/HomeHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeHeader.scss";
+
+function HomeHeader(){
+
+    return(
+    <>
+        <div className="home-header-container">
+        <div className="home-header-logo">Hello</div>
+        </div>
+    </>
+    );
+}
+
+export default HomeHeader;
--- client/src/Containers/Home.js
+++ client/src/Containers/Home.js
@@ -1,5 +1,5 @@
 import React from "react"
-import HomeHeader from "../Components/HomeHeader";
+import HomeHeader from "../Components/HomeHeader/HomeHeader";
 
 function Home(){
    return (
